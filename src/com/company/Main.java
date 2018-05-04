c15c53350d6d4cd242a07b056f29d793b987a513
dhj <547250674@qq.com>
2018-05-04 10:27:16 +0800

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello NOWCODER");
+        System.out.println("Hello NOWCODER1");
+        System.out.println("Hello NOWCODER2");
+
+
+    }
+}
